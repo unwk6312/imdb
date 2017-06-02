a18d9bc845b88952934d6c05de8b681870a9aecb
unwk6312 <internetworking123imbd@gmail.com>
2017-06-02 17:01:06 -0300

    first upload

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/labstudent/Documents/django_test/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ project1/polls/views.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import Question
+
+from django.template import loader
+
+from django.http import Http404
+# from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+
+
+def index(request):
+#     output = ', '.join([q.question_text for q in latest_question_list])
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+  
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
